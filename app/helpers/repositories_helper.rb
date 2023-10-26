909702bbc0f040f96ef280708e0de7f038e01a51
Talha86-ux <rjtalha555@gmail.com>
2023-10-27 01:17:54 +0500

    Fix bug in the multiselect and add a new column for repository users

--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -1,2 +1,7 @@
 module RepositoriesHelper
+
+  def display_repository_users(users)
+    user_ids = JSON.parse(users).reject(&:empty?).map(&:to_i)
+    user_ids.map { |user_id| User.find_by(id: user_id)&.first_name }
+  end
 end
