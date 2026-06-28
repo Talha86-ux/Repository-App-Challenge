8861797d0af373c27c4e4352bef6cfcd55a5c872
Talha86-ux <rjtalha555@gmail.com>
2026-06-28 22:51:00 +0500

    Fix multiple migration issues

--- db/migrate/20230916213519_create_repositories.rb
+++ db/migrate/20230916213519_create_repositories.rb
@@ -3,8 +3,6 @@ class CreateRepositories < ActiveRecord::Migration[6.1]
     create_table :repositories do |t|
       t.string :name
       t.text :description
-      t.string :owner
-      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
