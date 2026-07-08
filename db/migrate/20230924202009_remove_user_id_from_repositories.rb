894473b977e09c5e9fe8e3b28c2b52c71893ae96
Talha86-ux <rjtalha555@gmail.com>
2026-07-08 21:16:34 +0500

    Fix the migration

--- db/migrate/20230924202009_remove_user_id_from_repositories.rb
+++ db/migrate/20230924202009_remove_user_id_from_repositories.rb
@@ -1,5 +1,5 @@
 class RemoveUserIdFromRepositories < ActiveRecord::Migration[6.1]
   def change
-    remove_column :repositories, :user_id, :bigint
+    remove_column :repositories, :user_id, :bigint, if_exists: true
   end
 end
