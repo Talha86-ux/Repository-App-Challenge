c154e8fa96e3c2eaf2baba9db784084e4bb520c3
Talha86-ux <rjtalha555@gmail.com>
2026-07-09 01:39:37 +0500

    Add safe check

--- db/migrate/20230924202009_remove_user_id_from_repositories.rb
+++ db/migrate/20230924202009_remove_user_id_from_repositories.rb
@@ -1,5 +1,7 @@
 class RemoveUserIdFromRepositories < ActiveRecord::Migration[6.1]
   def change
-    remove_column :repositories, :user_id, :bigint, if_exists: true
+    if column_exists?(:repositories, :user_id)
+      remove_column :repositories, :user_id, :bigint
+    end
   end
 end
